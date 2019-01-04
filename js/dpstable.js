6056d32aed244ce5262bb2c7bc822fdca790a43f
ZenoraLagging <46304946+ZenoraLagging@users.noreply.github.com>
2019-01-04 13:44:53 -0800

    Add files via upload

--- js/dpstable.js
+++ js/dpstable.js
@@ -1,5 +1,45 @@
 $(document).ready(function() {
 	
-    $('#dpsTable').tablesorter();
+    $('#dpsTable').tablesorter({
+
+        theme: "bootstrap",
+        widthFixed: true,
+        widgets: ["filter"],
+
+        widgetOptions: {
+            // reset filters button
+            filter_reset: ".reset",
+
+            // extra css class name (string or array) added to the filter element (input or select)
+            filter_cssFilter: [
+                'form-control',
+                'form-control',
+                'form-control',
+                'form-control',
+                'form-control',
+                'form-control',
+                'form-control'
+            ]
+
+        }
+    })
+        .tablesorterPager({
+
+            // target the pager markup - see the HTML block below
+            container: $(".ts-pager"),
+
+            // target the pager page select dropdown - choose a page
+            cssGoto: ".pagenum",
+
+            // remove rows from the table to speed up the sort of large tables.
+            // setting this to false, only hides the non-visible rows; needed if you plan to add/remove rows with the pager enabled.
+            removeRows: false,
+
+            // output string - default is '{page}/{totalPages}';
+            // possible variables: {page}, {totalPages}, {filteredPages}, {startRow}, {endRow}, {filteredRows} and {totalRows}
+            output: '{startRow} - {endRow} / {filteredRows} ({totalRows})'
+
+        });
+
+});
 	
-} );
